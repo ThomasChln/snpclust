3350d1b8a18264d7e633a4707a7a5dfc9de33faf
Thomas Charlon <charlon@protonmail.com>
2019-09-12 17:13:14 +0200

    v0.2 code coverage travis 2

--- codecov.sh
+++ codecov.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 apt-get install -y git
-R -e "install.packages('covr'); covr::codecov(token = $CODECOV_TOKEN)"
+R -e "install.packages('covr'); covr::codecov(token = '$CODECOV_TOKEN')"
